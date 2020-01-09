3e36d05b5472203b86f42e3c46495cfa843de60a
O.K <okgit@protonmail.com>
2020-01-09 17:22:28 +0100

    Removed clutter and added docstrings

--- oblig1/oblig1.py
+++ oblig1/oblig1.py
@@ -2,14 +2,21 @@ import random
 import math
 
 class Queue:
-
-    def __init__(self, list=None):
+    """
+        En array-basert queue implementasjon. Læreboka anbefaler en annen implementasjon, men jeg så ingen grunn til å skrive noe annet enn det her for dette formålet.
+    """ 
+    def __init__(self, capacity=256, list=None):
         self.list = list
+        self.capacity = capacity
         if list is None:
             self.list = []
 
+    # Legger til en sjekk i add for å forsikre oss om at det ikke er mulig å legge til flere elementer om køen er full. Denne er strengt tatt ikke nødvendig i dette tilfellet, siden vi sjekker lengre ned, men future-proofing.
     def add(self, item):
-       self.list.append(item)
+        if len(self.list) < self.capacity:            
+            self.list.append(item)
+        else:
+            return False
 
     def peek(self):
         # Hvis lista er tom, er køen tom.
@@ -24,6 +31,9 @@ class Queue:
             item = self.list[0]
         self.list.remove(item)
 
+    def get_capacity(self):
+        return int(self.capacity)
+
     def size(self):
         return len(self.list.size)
 
@@ -36,8 +46,11 @@ class Queue:
         return iter(self.list)
 
 class Plane:
+    """
+        Klasse som egentlig bare skal holde på informasjon om fly. 
+        Genererer navn selv om ingenting blir spesifisert. Har pseudo-unique identifier.
+    """
 
-    # Konstruktør med default på navn. Genererer "random" navn om den er tom.
     def __init__(self, name=None):
         self.plane_types = ["Boeing 747", "Boeing 737 MAX", "Antonov An-2", "Solar Impulse 1"]
         if name is None:
@@ -46,19 +59,21 @@ class Plane:
             name = '{} [{}]'.format(self.plane_types[plane_type], pseudo_identifier) 
         self.name = name
 
-    @staticmethod 
-    def generate_random_identifiers():
-        letters = ''
-
+    # Overrider str så vi får navnet på flyet istedenfor en objekt-referanse når vi bruker print på et plane-objekt.
     def __str__(self):
         return self.name
 
 class Airport:
 
+    """
+        Klasse som simulerer handlingene til en flyplass, er også den som simulerer algoritmen.
+    """
+
+
     # Konstruktør med default tidsenheter
-    def __init__(self, mean, t=10):
+    def __init__(self, mean, time_laps=10):
         self.mean = mean 
-        self.t = t
+        self.time_laps = time_laps 
         self.total_planes_handled = 0
         self.total_planes_rejected = 0
         self.total_planes_accepted = 0
@@ -76,41 +91,42 @@ class Airport:
             k = k + 1
         return k - 1
 
+    # Initialiserer kjøringen av algoritmen. Oppretter kø-objekter og starter simuleringen.
     def initialize(self):
-        self.in_traffic = Queue()
-        self.on_ground = Queue()
-        self.time_step()
-        
+        self.in_traffic = Queue(20)
+        self.on_ground = Queue(20)
+        self.time_step() 
 
     def time_step(self):
-        for step in range(self.t):
+        for step in range(self.time_laps):
             self.generate_new_planes(self.in_traffic)
             self.generate_new_planes(self.on_ground)
+            # Prioriterer flyene i luften slik oppgaven ber om
             if self.in_traffic.peek():
-                self.in_traffic = self.handle_plane(self.in_traffic, 'landing', 'landet')
+               # self.in_traffic = 
+                self.handle_plane(self.in_traffic, 'landing', 'landet')
                 self.total_planes_landed += 1
             elif self.on_ground.peek():
-                self.on_ground = self.handle_plane(self.on_ground, 'avgang', 'tok av')
+               # self.on_ground = 
+                self.handle_plane(self.on_ground, 'avgang', 'tok av')
                 self.total_planes_take_off += 1
                 
     # queue er køen som blir brukt. Pre er før handling, post er etter.
     # Eks: pre = landing, post = landet
     # Fly 1 klar for _landing_
     # Fly 1 _landet_
-    def handle_plane(self, queue, pre, post) -> Queue: 
+    def handle_plane(self, queue, pre, post): 
         if queue.peek():
             plane = queue.peek()
             print("Fly {} klar for {}".format(plane, pre))
             queue.remove()
             print("Fly {} {}".format(plane, post))
-        return queue
 
     def generate_new_planes(self, queue):
-        amount = self.get_poisson_random(self.mean)
-        print("AMOUNT: " + str(amount))
-        for planes in range(amount):
+        amount_of_new_planes = self.get_poisson_random(self.mean)
+        for planes in range(amount_of_new_planes):
             plane = Plane()
-            if len(queue) < 11:
+            if len(queue) < queue.get_capacity():
                 queue.add(plane)
             else:
                 print("Køen er full")
