751797a2211bf76a39770ac6d5c2da183880ca41
O.K <okgit@protonmail.com>
2020-02-07 23:53:32 +0100

    Finished oblig 1. Added output

--- oblig/oblig1/oblig1.py
+++ oblig/oblig1/oblig1.py
@@ -1,49 +1,47 @@
 import random
 import math
 
-class Queue:
+class CircularQueue:
     """
-        En array-basert queue implementasjon. Læreboka anbefaler en annen implementasjon, men jeg så ingen grunn til å skrive noe annet enn det her for dette formålet.
-    """ 
-    def __init__(self, capacity=256, list=None):
-        self.list = list
-        self.capacity = capacity
-        if list is None:
-            self.list = []
+        https://www.pythoncentral.io/circular-queue/
+        Oppgaven linker til ferdiglagde klasser i Java, så håper det er greit å bruke ferdiglagde klasser uavhengig av språk.
+    """
+    #Constructor
+    def __init__(self):
+        self.queue = list()
+        self.head = 0
+        self.tail = 0
+        self.maxSize = 8
 
-    # Legger til en sjekk i add for å forsikre oss om at det ikke er mulig å legge til flere elementer om køen er full. Denne er strengt tatt ikke nødvendig i dette tilfellet, siden vi sjekker lengre ned, men future-proofing.
-    def add(self, item):
-        if len(self.list) < self.capacity:            
-            self.list.append(item)
-        else:
+    #Adding elements to the queue
+    def enqueue(self,data):
+        if self.size() == self.maxSize-1:
             return False
+        self.queue.append(data)
+        self.tail = (self.tail + 1) % self.maxSize
+        return True
+
+    #Removing elements from the queue
+    def dequeue(self):
+        if self.size()==0:
+            return ("Queue Empty!") 
+        data = self.queue[self.head]
+        self.head = (self.head + 1) % self.maxSize
+        return data
 
     def peek(self):
-        # Hvis lista er tom, er køen tom.
-        if not self.list:
-            return False
-        return self.list[0]
-    
-    def remove(self, item=None):
-        if not self.list:
-            return 'Køen er tom'
-        if item is None:
-            item = self.list[0]
-        self.list.remove(item)
+        if self.size() == 0:
+            return None
+        data = self.queue[self.head]
+        return data
 
-    def get_capacity(self):
-        return int(self.capacity)
 
+    #Calculating the size of the queue
     def size(self):
-        return len(self.list.size)
+        if self.tail>=self.head:
+            return (self.tail-self.head)
+        return (self.maxSize - (self.head-self.tail))
 
-    # Overrider len() metoden til Python slik at vi også kan bruke den (i tillegg til size) metoden for vår egen klasse.
-    def __len__(self):
-        return len(self.list)
-
-    # Overrider metoden som blir brukt i loops. Nå kan vi loope over lista til klassen.
-    def __iter__(self):
-        return iter(self.list)
 
 class Plane:
     """
@@ -71,15 +69,15 @@ class Airport:
 
 
     # Konstruktør med default tidsenheter
-    def __init__(self, mean, time_laps=10):
-        self.mean = mean 
+    def __init__(self, mean_landings=0.6, mean_take_off=0.4, time_laps=10):
+        self.mean_landings = mean_landings
+        self.mean_take_off = mean_take_off
         self.time_laps = time_laps 
         self.total_planes_handled = 0
         self.total_planes_rejected = 0
         self.total_planes_accepted = 0
         self.total_planes_take_off = 0
         self.total_planes_landed = 0
-        print(self.mean)
 
     def get_poisson_random(self, mean):
         r = random
@@ -93,14 +91,16 @@ class Airport:
 
     # Initialiserer kjøringen av algoritmen. Oppretter kø-objekter og starter simuleringen.
     def initialize(self):
-        self.in_traffic = Queue(20)
-        self.on_ground = Queue(20)
+        self.in_traffic = CircularQueue()
+        self.on_ground = CircularQueue()
         self.time_step() 
 
     def time_step(self):
         for step in range(self.time_laps):
-            self.generate_new_planes(self.in_traffic)
-            self.generate_new_planes(self.on_ground)
+            separator = "[Tidssteg {}]".format(step)
+            print(separator.center(58, '-'))
+            self.generate_new_planes(self.in_traffic, self.mean_landings)
+            self.generate_new_planes(self.on_ground, self.mean_take_off)
             # Prioriterer flyene i luften slik oppgaven ber om
             if self.in_traffic.peek():
                # self.in_traffic = 
@@ -110,7 +110,29 @@ class Airport:
                # self.on_ground = 
                 self.handle_plane(self.on_ground, 'avgang', 'tok av')
                 self.total_planes_take_off += 1
-                
+            else:
+                print("Ingenting å gjøre".center(58, " "))
+            print("\n") 
+        self.print_results()
+
+
+    def print_results(self):
+        print("[Resultat]".center(58, "-"))
+        self.pretty_print("Simlueringen ferdig etter", "{} tidsenheter".format(self.time_laps))
+        self.pretty_print("Totalt antall fly behandlet", self.total_planes_handled)
+        self.pretty_print("Antall fly landet", self.total_planes_landed)
+        self.pretty_print("Antall fly tatt av", self.total_planes_take_off)
+        self.pretty_print("Antall fly avvist", self.total_planes_rejected)
+        self.pretty_print("Antall fly klare til landing", self.in_traffic.size())
+        self.pretty_print("Antall fly klare til take off", self.on_ground.size())
+        print("[Slutt]".center(58, "-"))
+        
+        
+    def pretty_print(self, first, second):
+        printer = [str(first).ljust(30, " "), str(second).ljust(3, " ")]
+        print(" {} : {}".format(printer[0], printer[1]))
+
+
     # queue er køen som blir brukt. Pre er før handling, post er etter.
     # Eks: pre = landing, post = landet
     # Fly 1 klar for _landing_
@@ -118,24 +140,36 @@ class Airport:
     def handle_plane(self, queue, pre, post): 
         if queue.peek():
             plane = queue.peek()
-            print("Fly {} klar for {}".format(plane, pre))
-            queue.remove()
-            print("Fly {} {}".format(plane, post))
+            print("Fly {} klar for {}".format(plane, pre).center(58, " "))
+            queue.dequeue()
+            print("Fly {} {}".format(plane, post).center(58, " "))
 
-    def generate_new_planes(self, queue):
-        amount_of_new_planes = self.get_poisson_random(self.mean)
+    def generate_new_planes(self, queue, mean):
+        amount_of_new_planes = self.get_poisson_random(mean)
         for planes in range(amount_of_new_planes):
             plane = Plane()
-            if len(queue) < queue.get_capacity():
-                queue.add(plane)
-            else:
-                print("Køen er full")
+            success = queue.enqueue(plane)
+            if not success:
                 self.total_planes_rejected += 1
+                break
             self.total_planes_handled += 1
 
 
+try:
 
-a = Airport(0.3, 20)
+    time_laps = int(input("[Integer] Hvor mange tidsenheter skal simuleringen gå? : "))
+    take_offs_per_time_lap = float(input("[Float < 1.0] Forventet antall ankomster pr. tidsenhet? : "))
+    landings_per_time_lap = float(input("[Float < 1.0] Forventet antall avganger pr. tidsenhet? : "))
+except: 
+    time_laps = 20
+    take_offs_per_time_lap = 0.6
+    landings_per_time_lap = 0.4
+    print("Noe gikk galt med inputs. Bruker defaults")
+
+if take_offs_per_time_lap > 0.9 or landings_per_time_lap > 0.9:
+    exit("Forventet antall * pr. tidsenhet kan ikke være høyere enn 0.9")
+
+a = Airport(take_offs_per_time_lap, landings_per_time_lap, time_laps)
 a.initialize()
 
 
